a37c3398911cad1e5bd94c4cba9397c65843c952
FalendraDewangan <falendra78@gmail.com>
2020-01-24 16:18:27 +0530

    permutation and combination cpp program

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,15 +3,80 @@ using namespace std;
 
 int permutation(int n, int r)
 {
+	if(n==0||r==0)
+		return 1;
+	else if(r==0)
+		return n;
+	else
+	{
+		return n*permutation(n-1,r-1);
+	}
+	
   return 0;
 }
 
 int combination(int n, int r)
 {
+	if(n==0||r==0)
+		return 1;
+	else if(r==0)
+		return n;
+	else
+	{
+		return 	n*combination(n-1,r-1)/r;
+	}
+	
   return 0;
 }
 
 int main()
 {
+	int n,r,choice;
+	char y;
+	cout<<"Enter the value n : ";
+	cin>>n;
+	cout<<"Enter the value r : ";
+	cin>>r;
+	try{
+	if(n>0 && r>0 && n>r)
+	{
+	
+	do
+	{
+	cout<<"Enter 1 for permutation: \n";
+	cout<<"Enter 2 for combination : ";
+
+	cout<<"\n Enter your choice: ";
+	cin>>choice;
+
+	switch(choice)
+	{
+		case 1:
+			if(permutation(n,r)>0)
+			cout<<permutation(n,r);
+			break;
+		case 2:
+			if(combination(n,r)>0)
+			cout<<combination(n,r);
+			break;
+	
+		default :
+			cout<<"Invalid choice";
+	}
+	
+	cout<<"\n Do you want to continue y/n ";
+	cin>>y;
+	
+	}while(y=='y');
+}
+else
+	{
+		throw -1;
+	}
+}
+catch(int y)
+{
+	cout<<"Not defined for your input ";
+}
   return 0;
 }
